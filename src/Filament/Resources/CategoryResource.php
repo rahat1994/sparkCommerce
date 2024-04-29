1361aa7eb16adcfd3a5ba7235dc4fb242990216d
rahat1994 <rahat1994@users.noreply.github.com>
2024-04-29 13:30:32 +0000

    Fix styling

--- src/Filament/Resources/CategoryResource.php
+++ src/Filament/Resources/CategoryResource.php
@@ -3,14 +3,10 @@
 namespace App\Filament\Resources;
 
 use App\Filament\Resources\CategoryResource\Pages;
-use App\Filament\Resources\CategoryResource\RelationManagers;
-use Filament\Forms;
 use Filament\Forms\Form;
 use Filament\Resources\Resource;
 use Filament\Tables;
 use Filament\Tables\Table;
-use Illuminate\Database\Eloquent\Builder;
-use Illuminate\Database\Eloquent\SoftDeletingScope;
 use Rahat1994\SparkCommerce\Models\SCCategory;
 
 class CategoryResource extends Resource
