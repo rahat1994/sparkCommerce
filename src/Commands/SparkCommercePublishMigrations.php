8f4b5f93a739435729e9f545027b3c5c490407b1
Rahat Baksh <rahat392@gmail.com>
2024-05-06 16:34:28 +0600

    adding spatie media library and migration publishing.

--- src/Commands/SparkCommercePublishMigrations.php
+++ src/Commands/SparkCommercePublishMigrations.php
@@ -4,6 +4,7 @@ namespace Rahat1994\SparkCommerce\Commands;
 
 use Illuminate\Console\Command;
 use Rahat1994\SparkCommerce\SparkCommerceServiceProvider;
+use Spatie\MediaLibrary\MediaLibraryServiceProvider;
 use Spatie\Tags\TagsServiceProvider;
 
 class SparkCommercePublishMigrations extends Command
@@ -22,6 +23,11 @@ class SparkCommercePublishMigrations extends Command
         $this->call('vendor:publish', [
             '--provider' => TagsServiceProvider::class,
             '--tag' => 'tags-migrations',
+        ]);
+
+        $this->call('vendor:publish', [
+            '--provider' => MediaLibraryServiceProvider::class,
+            '--tag' => 'medialibrary-migrations',
         ]);
 
         $this->info('All migrations have been published successfully.');
